c7782218f445d4c9bcc72dfd21ad2ab244c7041e
davidlale <mrdavidlale@gmail.com>
2021-07-24 06:36:14 +0100

    Add Humdity reading to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location - try another search', undefined)
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. There is a ' + body.current.precip + '% chance of rain and humidity is running at ' + body.current.humidity)
     }
   })
 
